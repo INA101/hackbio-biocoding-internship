e876921b4e1bcc05e3709f184768dfc687c6c2a1
INA101 <noelemadan@gmail.com>
2025-02-16 20:12:15 +0100

    Added DNA to Protein function

---
+++ dna_to_protein.py
@@ -0,0 +1,39 @@
+def dna_to_protein(dna):
+    """Converts a DNA sequence into a protein sequence."""
+    # Dictionary of codons to amino acids
+    codon_table = {
+        'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
+        'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
+        'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',
+        'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',
+        'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+        'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+        'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+        'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
+        'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+        'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+        'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+        'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+        'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+        'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+        'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
+    }
+
+    # Step 1: Convert DNA to mRNA
+    rna = dna.replace('T', 'U')
+
+    # Step 2: Split into codons (3 bases per codon)
+    protein = ''
+    for i in range(0, len(rna), 3):
+        codon = rna[i:i + 3]
+        if codon in codon_table:
+            protein += codon_table[codon]
+
+    return protein
+
+
+# Example usage:
+dna_sequence = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
+protein_sequence = dna_to_protein(dna_sequence)
+print("Protein sequence:", protein_sequence)
